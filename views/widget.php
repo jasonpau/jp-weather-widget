2791418efd4f1e37ae846f66bbd40a459b8c9a4e
Jason Pau <jason@jasonpau.com>
2021-08-02 11:00:51 -0700

    Fixed issue with insecure element warning

--- views/widget.php
+++ views/widget.php
@@ -13,7 +13,7 @@ if ( $weather->cod === 200 ) :
 <div class="jp-weather-widget-container">
     <?php if ( $weather->weather[0] ) : ?>
         <img class="jp-weather-widget-icon"
-            src="http://openweathermap.org/img/wn/<?= esc_attr( $weather->weather[0]->icon ) ?>@2x.png"
+            src="https://openweathermap.org/img/wn/<?= esc_attr( $weather->weather[0]->icon ) ?>@2x.png"
             alt="<?= esc_attr( $weather->weather[0]->description ) ?>">
     <?php endif; ?>
     <?php if ( $weather?->main?->temp ) : ?>
